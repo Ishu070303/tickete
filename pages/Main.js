52bdade3f837d2b827e00eec888e6d74ab9d46a2
Eshika rawat <eshikarawat02@gmail.com>
2023-07-25 17:35:32 +0530

    added styling to payment section

--- pages/Main.js
+++ pages/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { TextField, FormControl, Select, MenuItem, InputLabel } from '@mui/material';
+import { TextField, FormControl, Select, MenuItem, InputLabel, Paper} from '@mui/material';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
 
 
 
@@ -51,8 +53,29 @@ const Main = () => {
     </div>
 
     {/*Payment Mode*/}
-    <h5 className='font-semibold text-2xl mt-6' style={{ color: '#474747'}}>Additional information</h5>
-    <p className='text-base mt-1' style={{ color: '#595959'}}>We need a few more details to complete your reservation.</p>
+    <h5 className='font-semibold text-2xl mt-8' style={{ color: '#474747'}}>Select your mode of payment</h5>
+    <p className='text-base mt-1' style={{ color: '#595959'}}>Payments with Tickete are secure and encrypted.</p>
+    
+    <Paper style={{width: '47vw', marginTop: '1rem', padding: '5px'}}>
+        <div className='flex justify-between font-semibold text-base' style={{ color: "#474747", }}>
+            <div className='flex justify-between' style={{width: '11rem'}}>
+               <CreditCardIcon/>
+               <h6>Credit & debit card</h6>
+            </div>
+            <div>
+                <RadioButtonCheckedIcon style={{ cursor: 'pointer'}} />
+            </div>
+        </div>
+        <div className='flex flex-wrap mt-5' style={{ width: '47vw'}}>
+            <TextField style={{width: '22.7vw', color: '#D6D6D6'}} className='my-3 mr-2 ml-1'  id="outlined-required" label="Card Holder's Name" defaultValue="Eshika Rawat" />
+            <TextField style={{width: '22.7vw', color: '#D6D6D6'}} className='my-3 mr-2'  id="outlined-required" label="Card Number" defaultValue="43879151420781" />
+            <TextField style={{width: '22.7vw', color: '#D6D6D6'}} className='my-3 mr-2 ml-1'  id="outlined-required" label="Expiry Date" defaultValue="06/27" />
+            <TextField style={{width: '22.7vw', color: '#D6D6D6'}} className='my-3 mr-2'  id="outlined-error-helper-text" label="CW/CVC" defaultValue="***"/>
+        </div>
+    </Paper>
+    <Paper>
+        
+    </Paper>
 
     </>
   )
