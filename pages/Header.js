db54cb0980abefec11b58cecbbb7a995234e9104
Eshika rawat <eshikarawat02@gmail.com>
2023-07-26 14:35:34 +0530

    donw with header makestyles

--- pages/Header.js
+++ pages/Header.js
@@ -1,15 +1,70 @@
 import React from 'react';
-import { QuestionIcon } from '@chakra-ui/icons';
+import { QuestionIcon} from '@chakra-ui/icons';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '80vw',
+      margin: 'auto',
+      marginTop: '1rem',
+      [theme.breakpoints.down('md')]:{
+        display: 'none'
+      }
+    },
+
+    head: {
+      fontWeight: 'bold',
+      fontSize: '20px'
+    },
+
+    containerDiv: {
+      color: '#474747',
+      fontSize: '16px'
+    },
+
+    icon: {
+      cursor: 'pointer',
+      color: '#474747',
+      marginRight: '.2rem'
+    },
+
+    leftIcon: {
+         margin: 'auto'
+    },
+
+    iconDiv: {
+      display: 'none',
+      [theme.breakpoints.down('md')]: {
+        display: 'block',
+        marginTop: '1rem',
+        width: '6vw',
+        height: '3vh',
+        backgroundColor : '#474747',
+        color: 'white',
+        borderRadius: '100%',
+        alignItems: 'center',
+        marginLeft: '2rem'
+      }
+    }
+}));
 
 const Header = () => {
+
+  const classes = useStyles();
+
   return (
     <>
-     <div className='flex justify-between' style={{ width: '80vw', margin: 'auto', marginTop : '1rem'}}>
-        <div style={{fontWeight: 'bold', fontSize: '20px' }}>
+     <div className={classes.iconDiv}><KeyboardArrowLeftIcon/></div>
+     <div className={classes.container}>
+        <div className={classes.head}>
             tickete
         </div>
-        <div style={{color:'#474747', fontSize: '16px'}}>
-            <QuestionIcon style={{ cursor : 'pointer', color: '#474747', marginRight: '.2rem'}}/>
+        <div className={classes.containerDiv}>
+            <QuestionIcon className={classes.icon}/>
             Help
         </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps}/></ThemeProvider>
 }
