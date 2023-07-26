8f2aa3c99abd663a0ba2d962a37586144293b258
Eshika rawat <eshikarawat02@gmail.com>
2023-07-27 04:46:14 +0530

    added makestyles in section file

--- pages/Footer.js
+++ pages/Footer.js
@@ -10,6 +10,83 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 import { CardContent, CardMedia, Card, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    FooterDiv: {
+        width: '80vw', 
+        margin: 'auto', 
+        marginTop: '4rem',
+
+        [theme.breakpoints.down('md')]: {
+            marginTop: '2rem',
+        }
+    },
+
+    FooterDivh4: {
+        color: '#474747',
+        fontSize: '24px',
+        fontWeight: '600',
+
+        [theme.breakpoints.down('md')]: {
+            fontSize: '18px'
+        }
+    },
+
+    FooterDivChild: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: '1.25rem',
+
+        [theme.breakpoints.down('md')]: {
+        //    border: '2px solid black',
+           display: 'block',
+           width: '80vw',
+           marginTop:'1rem'
+        }
+    },
+
+    FooterCard: {
+        width: '20rem', 
+        border: '1px solid #D6D6D6',
+        marginBottom: '2rem',
+        marginRight: '0.5rem',
+
+        [theme.breakpoints.down('md')]: {
+            width: '80vw',
+            display: 'flex', 
+            alignItems: 'center'
+        }
+    },
+
+    FooterCardMedia: {
+        marginLeft: '2rem',
+        marginTop: '.5rem',
+
+        [theme.breakpoints.down('md')]: {
+            marginLeft: '.2rem',
+            marginTop : 0
+        }
+    },
+
+    FooterType1: {
+        color: '#474747',
+        fontWeight: '600',
+        fontSize: '16px'
+    },
+
+    FooterType2: {
+        color: '#474747', 
+        width: '74%',
+        fontWeight: '400',
+        fontSize: '16px',
+
+        [theme.breakpoints.down('md')]: {
+            fontSize: '14px', 
+            width: '75vw'
+        }
+    }
+}));
 
 const Footer = () => {
 
@@ -32,23 +109,29 @@ const Footer = () => {
     }
   }
 
+  const classes = useStyles();
+
   return (
+    
     <>
-    <div style={{width: '80vw', margin: 'auto', marginTop: '4rem'}}>
-        <h4 className='font-semibold text-2xl' style={{ color: '#474747'}}>The Tickete promise</h4>
-        <div className='flex justify-between mt-5'>
+    {/*=========================== POLICY SECTION FOR BIG AND SMALL SCREENS ================================= */}
+    <div className={classes.FooterDiv}>
+        <h4 className={classes.FooterDivh4}>The Tickete promise</h4>
+        <div className={classes.FooterDivChild}>
           {
             trust.map((item, index) => (
-                <Card key={index} className= 'mb-4 mr-2' style={{ width: '20rem', border: '1px solid #D6D6D6'}}>
-                    <CardMedia className='ml-4 mt-2'>{getIconComponent(item.img, item.color)}</CardMedia>
+                <Card key={index} className= {classes.FooterCard}>
+                    <CardMedia className={classes.FooterCardMedia}>{getIconComponent(item.img, item.color)}</CardMedia>
                     <CardContent>
-                        <Typography style={{color: '#474747'}} className='font-semibold text-base' variant='h6'>{item.title}</Typography>
-                        <Typography style={{color: '#474747', width: '74%'}} className='font-normal text-base' variant='body2'>{item.desc}</Typography>
+                        <Typography className={classes.FooterType1} variant='h6'>{item.title}</Typography>
+                        <Typography className={classes.FooterType2} variant='body2'>{item.desc}</Typography>
                     </CardContent>
                 </Card>
             ))}
         </div>
     </div>
+
+    {/*=========================== FOOTER END SECTION FOR BIG AND SMALL SCREENS ================================= */}
     <div className='mt-5' style={{ backgroundColor: 'black', height: '30vh'}}>
         <Typography className='py-9' style={{color: 'white', width: '90vw', margin:'auto', }} variant='h3'>tickete</Typography>
 
